77dc9925d8f320190deaeeb09f058b2c1ed215e8
Manjul Singh Sachan <sachanmanjul@gmail.com>
2021-12-01 21:03:15 -0500

    initial visualization of top 25 artists with ReactJs and D3.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import BarChart from './charts/BarChart';
+import * as d3 from 'd3';
+
+var rawData = {};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        loadData();
+    }, []);
+
+    const loadData = () => {
+        d3.json("/StreamingHistory0.json").then(function(d) {
+            debugger;
+            analyzeData(d);
+        })
+    }
+
+    const analyzeData = (raw) => {
+        var cume_artist_time = {};
+
+        var temp_name, temp_time = null;
+        for(var i=0; i < raw.length; i++){
+            temp_name = raw[i]["artistName"];
+            temp_time = parseInt(raw[i]["msPlayed"])/1000.0/60.0/60.0;
+            if(temp_name in cume_artist_time){
+                cume_artist_time[temp_name] = cume_artist_time[temp_name] + temp_time;
+            }
+            else { 
+                cume_artist_time[temp_name] = temp_time;
+            }
+        }
+
+        var cume_artist_time_arr = [];
+
+        Object.keys(cume_artist_time).forEach((key) => cume_artist_time_arr.push(
+            {
+                artist: key,
+                total_time: cume_artist_time[key]
+            }));
+        
+
+
+        var graphData = cume_artist_time_arr;
+
+
+        graphData.sort((a,b) => d3.descending(a.total_time, b.total_time));
+
+        graphData = graphData.slice(0,25);
+
+        setData(graphData);
+    }
+
+    // useEffect(() => {
+    //     changeData();
+    // }, [rawData]);
+
+    // const changeData = () => {
+    //     setData(datas[i++]);
+    //     if(i === datas.length) i = 0;
+    // }
+
+
+    return (
+        <div className="App" style={{backgroundColor: '#191414', color: '#1DB954'}}>
+            <h2>Top 25 Spotify Artists</h2>
+            {/* <button onClick={changeData}>Change Data</button> */}
+            <BarChart width={800} height={600} data={data} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/charts/BarChart.js
@@ -0,0 +1,138 @@
+import * as d3 from 'd3';
+import React, { useRef, useEffect } from 'react';
+
+function BarChart({ width, height, data }){
+    const ref = useRef();
+
+    var margin = ({top: 20, right: 120, bottom: 30, left: 120})
+    var font = 'OpenSans'
+
+    useEffect(() => {
+        const svg = d3.select(ref.current)
+            .attr("width", width)
+            .attr("height", height)
+    }, []);
+
+    useEffect(() => {
+        draw();
+    }, [data]);
+
+    const draw = () => {
+        
+        const svg = d3.select(ref.current);
+
+        var scaleArtistY = d3.scaleBand()
+            .domain(data.map((d)=>d.artist))
+            .range([margin.top, height-margin.bottom])
+            .padding(.1)
+
+        var scaleAmountX = d3.scaleLinear()
+            .domain([0, d3.max(data, (d,i) => d.total_time)])
+            .range([margin.left, width-margin.right])
+            .nice();
+            //nice rounds off values
+
+        var scaleArtistColor = 
+            d3.scaleOrdinal(d3.quantize(d3.interpolateWarm, 26))
+
+        var yAxis = d3.axisLeft(scaleArtistY)
+
+        var xAxis = d3.axisBottom(scaleAmountX)
+            .tickFormat(val => val + " hrs")
+            .ticks(4);
+
+        svg.append('g')
+            .style("font-family", font)
+            .style("font-size", "0.8vw")
+            .attr('class', 'y axis')
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(yAxis);
+        
+        svg.append("g")
+            .style("font-family", font)
+            .style("font-size", "1vw")
+            .attr('class', 'x axis')
+            .attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(xAxis);
+
+        var barg = svg.append("g");
+
+        var rects = barg
+            .selectAll("rect")
+            .data(data);
+        
+        var newRects = rects.enter();
+        
+        var texts = barg
+            .selectAll("text")
+            .data(data)
+        
+        var newTexts = texts.enter();
+
+        newRects
+            .append("rect")
+                .attr("class", "rect")
+                .attr("id", (d,i) => i)
+                .attr("x", margin.left + 2)
+                .attr("y", (d,i) => scaleArtistY(d.artist))
+                .attr("width", 0)
+                .attr("height", (d,i) => scaleArtistY.bandwidth())
+                .attr("fill", (d,i) => scaleArtistColor(d.total_time))
+                .attr("opacity", 1)
+
+        barg.selectAll("rect")
+            .transition()
+            .duration(1000)
+            .attr("width", (d,i) => scaleAmountX(d.total_time) - margin.left)
+
+        newTexts
+            .append("text")
+                .attr("text-anchor", "end")
+                .attr("y", (d,i) => scaleArtistY(d.artist) + scaleArtistY.bandwidth()/2.0 +5)
+                .attr("x", (d,i) => scaleAmountX(d.total_time))
+                .text((d,i) => d3.format(".1f")(d.total_time))
+                .attr("fill", "white")
+                .style("font-family", font)
+
+        // var selection = svg.selectAll("rect").data(data);
+        // var yScale = d3.scaleLinear()
+        //                     .domain([0, d3.max(data)])
+        //                     .range([0, height-100]);
+        
+        // selection
+        //     .transition().duration(300)
+        //         .attr("height", (d) => yScale(d))
+        //         .attr("y", (d) => height - yScale(d))
+
+        // selection
+        //     .enter()
+        //     .append("rect")
+        //     .attr("x", (d, i) => i * 45)
+        //     .attr("y", (d) => height)
+        //     .attr("width", 40)
+        //     .attr("height", 0)
+        //     .attr("fill", "orange")
+        //     .transition().duration(300)
+        //         .attr("height", (d) => yScale(d))
+        //         .attr("y", (d) => height - yScale(d))
+        
+        // selection
+        //     .exit()
+        //     .transition().duration(300)
+        //         .attr("y", (d) => height)
+        //         .attr("height", 0)
+        //     .remove()
+    }
+
+
+    return (
+        <div className="chart">
+            <svg ref={ref}>
+            </svg>
+        </div>
+        
+    )
+
+}
+
+export default BarChart;
